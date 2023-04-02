8be7dcea3a0b3a417ca2ef5a2adbdaf9bc5d315f
danpeczek <danpeczek@gmail.com>
2023-04-02 13:52:12 +0200

    wrapped imgui as cmake module
    * module and license
    
    * copy

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # imgui-cmake
 Wrapper for imgui library that makes it a legit CMake component 
+
+# Motivation
+
+To reduce copy-pastes when I want to use explicitly in my CMakes.
+
+# Usage
+If you don't want to use the version used here (0.11.3):
+* The `ocornut-imgui` directory contains the module to the original imgui repository. 
+* Type `git submodule update` to download it for your machine.
+* Checkout the commit you want to have.
+* Run `setup-imgui-allegro.bat` or `setup-imgui-allegro.sh` from the repo directory, to copy the files with proper allegro context (currentlly only allegro, because I'm using this one).
+
+For other usages just make it a submodule in your project and include it's directory in your project.
+
+# License
+License is the same as in the imgui library by Omar Cornut. Please refer to the LICENSE file for further details.
---
+++ setup-imgui-allegro.bat
@@ -0,0 +1,28 @@
+rd /S /Q .\imgui-allegro\include\
+rd /S /Q imgui-allegro\src\
+
+mkdir .\imgui-allegro\include
+mkdir .\imgui-allegro\include\imgui
+mkdir .\imgui-allegro\src
+
+set ORIGINAL_IMGUI_DIR=.\ocornut-imgui\imgui
+set TARGET_IMGUI_DIR=.\imgui-allegro\
+
+copy %ORIGINAL_IMGUI_DIR%\imgui.h %TARGET_IMGUI_DIR%\include\imgui\imgui.h
+copy %ORIGINAL_IMGUI_DIR%\imgui.cpp %TARGET_IMGUI_DIR%\src\imgui.cpp
+
+copy %ORIGINAL_IMGUI_DIR%\backends\imgui_impl_allegro5.h %TARGET_IMGUI_DIR%\include\imgui\imgui_impl_allegro5.h
+copy %ORIGINAL_IMGUI_DIR%\backends\imgui_impl_allegro5.cpp %TARGET_IMGUI_DIR%\src\imgui_impl_allegro5.cpp
+
+copy %ORIGINAL_IMGUI_DIR%\examples\example_allegro5\imconfig_allegro5.h %TARGET_IMGUI_DIR%\include\imgui\imconfig_allegro5.h
+
+copy %ORIGINAL_IMGUI_DIR%\imconfig.h %TARGET_IMGUI_DIR%\include\imgui\imconfig.h
+copy %ORIGINAL_IMGUI_DIR%\imgui_internal.h %TARGET_IMGUI_DIR%\include\imgui\imgui_internal.h
+copy %ORIGINAL_IMGUI_DIR%\imstb_rectpack.h %TARGET_IMGUI_DIR%\include\imgui\imstb_rectpack.h
+copy %ORIGINAL_IMGUI_DIR%\imstb_textedit.h %TARGET_IMGUI_DIR%\include\imgui\imstb_textedit.h
+copy %ORIGINAL_IMGUI_DIR%\imstb_truetype.h %TARGET_IMGUI_DIR%\include\imgui\imstb_truetype.h
+
+copy %ORIGINAL_IMGUI_DIR%\imgui_demo.cpp %TARGET_IMGUI_DIR%\src\imgui_demo.cpp
+copy %ORIGINAL_IMGUI_DIR%\imgui_draw.cpp %TARGET_IMGUI_DIR%\src\imgui_draw.cpp
+copy %ORIGINAL_IMGUI_DIR%\imgui_widgets.cpp %TARGET_IMGUI_DIR%\src\imgui_widgets.cpp
+copy %ORIGINAL_IMGUI_DIR%\imgui_tables.cpp %TARGET_IMGUI_DIR%\src\imgui_tables.cpp
